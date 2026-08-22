e4ea559d1428b74c8793c3139c7ac56d3639cba5
degas4728-design <degas4728@gmail.com>
2026-08-22 13:19:37 +0300

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-# config.py
-DSN = "postgresql://postgres:1234@localhost:5432/Database for image"
-
-HTTP_TIMEOUT = 30
-USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
-DOWNLOAD_DIR = './downloads'
