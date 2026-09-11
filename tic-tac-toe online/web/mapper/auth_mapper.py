00b2debca9d261edb3c782aa76f591dc0c6389e9
degas4728-design <user@DESKTOP-1ET9L8P.localdomain>
2026-09-11 10:29:37 +0300

    clear_develop_branch

--- tic-tac-toe online/web/mapper/auth_mapper.py
+++
@@ -1,9 +0,0 @@
-from web.model.auth_model import SignUpRequest
-
-class AuthMapper:
-    @staticmethod
-    def signup_request_to_user_data(request: SignUpRequest):
-        return {
-            "login": request.login,
-            "password": request.password
-        }
